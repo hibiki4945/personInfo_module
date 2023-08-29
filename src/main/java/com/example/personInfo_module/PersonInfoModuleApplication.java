0873e04f561a9f716673ab01d2bea1cf921fb735
hibiki4945 <wc4910720@gmail.com>
2023-08-29 09:16:52 +0800

    original

---
+++ src/main/java/com/example/personInfo_module/PersonInfoModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.personInfo_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonInfoModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonInfoModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personInfo_module/PersonInfoModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personInfo_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonInfoModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
